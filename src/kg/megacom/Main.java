677b9ed7f2b52192cb8288c9c4e0c1674fe561c9
Nursultan <nursultankudajkulov11@gmail.com>
2021-06-18 18:34:31 +0600

    linkedList methods

--- src/kg/megacom/Main.java
+++ src/kg/megacom/Main.java
@@ -1,16 +1,13 @@
 package kg.megacom;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> bookList = new ArrayList<>();
+        LinkedList<String> bookList = new LinkedList<>();
 
-        Collections.addAll(bookList, "Little Prince", "Alchemist", "11/22/63", "Diamond chariot", "Theory of everything");
+        /*Collections.addAll(bookList, "Little Prince", "Alchemist", "11/22/63", "Diamond chariot", "Theory of everything");
         System.out.println(bookList);
 
         bookList.remove(4);
@@ -18,11 +15,22 @@ public class Main {
 
         bookList.set(3, "Theory of everything");
         System.out.println(bookList);
-        System.out.println(bookList.get(3));
+        System.out.println(bookList.get(3));*/
 
         bookList.clear();
 
         Collections.addAll(bookList, "Sherlock Holmes", "Harry Potter", "Martian");
+        System.out.println(bookList);
+
+        bookList.addLast("War and peace");
+        bookList.addLast("War and peace");
+
+        bookList.addFirst("Birdsong");
+        bookList.addFirst("Birdsong");
+        System.out.println(bookList);
+
+        bookList.pollFirst();
+        bookList.pollLast();
         System.out.println(bookList);
     }
 }
